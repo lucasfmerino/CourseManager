906fe33277f11ae15d6abed45ff4022210ab5b14
lucasfmerino <lucasfmerino@gmail.com>
2023-11-22 01:42:25 -0300

    feat(Curso) Curso model added

---
+++ src/tests/TestaCurso.java
@@ -0,0 +1,21 @@
+package tests;
+
+import models.Aula;
+import models.Curso;
+
+public class TestaCurso {
+    public static void main(String[] args) {
+        Curso javaColecoes = new Curso("Dominando Coleções", "Lucas");
+
+        // javaColecoes.getAulas().add(new Aula("Trabalhando com ArrayList", 21));
+
+        javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+        javaColecoes.adiciona(new Aula("Criando uma Classe", 20));
+        javaColecoes.adiciona(new Aula("Modelando com Coleções", 22));
+
+        System.out.println(javaColecoes.getAulas());
+
+
+        
+    }
+}
